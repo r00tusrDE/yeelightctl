26f768fe4d73e0c8c328e37fa189d33bb5d9c9a7
Dominik P <mail@d-pelz.de>
2020-03-15 14:27:21 +0100

    yeelightctl can now handle variable names for the bulb IPs

--- yeelightctl.py
+++ yeelightctl.py
@@ -1,16 +1,27 @@
 import sys
+import csv
 from yeelight import Bulb
 
 if len(sys.argv) != 3:
 	print("Not enough parameters given. Please use:")
-	print("python yeelightctl.py <bulb_name> <on|off>")
+	print("python yeelightctl.py <bulb_ip> <on|off>")
 	sys.exit(0)
-
-if sys.argv[1] == "st":
-	bulb = Bulb("192.168.178.60")
-elif sys.argv[1] == "tv":
-	bulb = Bulb("192.168.178.61")
 	
+name_found = False
+	
+try:
+	with open('bulbs.csv', mode='r') as bulbs_csv:
+		csv_reader = csv.DictReader(bulbs_csv)
+		for row in csv_reader:
+			if sys.argv[1] == row["name"]:
+				bulb = Bulb(row["ip"])
+				name_found = True
+except Exception as ex:
+	print(ex)
+
+if name_found == False:
+	bulb = Bulb(sys.argv[1])
+
 if sys.argv[2] == "on":
 	try:
 		bulb.turn_on()
